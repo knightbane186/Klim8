35cda326b81bfea251ef36d05dc32e472d661ea4
knightbane186 <denverlopes93@gmail.com>
2024-03-19 18:49:49 +1100

    maintanencace

--- Model/WeatherData.swift
+++ Model/WeatherData.swift
@@ -21,3 +21,17 @@ struct Weather: Codable {
     let description: String
     let id: Int
 }
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
